f5578793095efe4994bec830c0ad1b8e9683b035
CyberWarriorGuru <mrogolino976@outlook.com>
2023-04-25 10:58:44 -0400

    test commit

---
+++ android/app/src/main/kotlin/com/example/nexusapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nexusapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/sign_in_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NexusApp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: SignInScreen()
+    );
+  }
+}
